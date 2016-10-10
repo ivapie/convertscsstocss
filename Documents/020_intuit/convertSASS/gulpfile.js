a6505fa096847967acde3ca106e81c613014704a
Ivan Piedra <ipiedra@cecropiasolutions.com>
2016-10-10 15:03:38 -0600

    init

---
+++ Documents/020_intuit/convertSASS/gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+let gulp = require("gulp");
+let minifycss = require("gulp-minify-css");
+let cleanCSS = require("gulp-clean-css");
+let concat = require("gulp-concat");
+var dedupe = require('gulp-dedupe');
+let sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./components/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('styles.css'))
+        .pipe(minifycss())
+        .pipe(dedupe())
+        .pipe(cleanCSS({debug: true}, function(details){
+            console.log(details);
+        }))
+        .pipe(gulp.dest('build/'));
+});
