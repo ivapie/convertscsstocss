b502e9d57c7b8fb27ead921ec2863eb17236a2cf
Ivan Piedra <ipiedra@cecropiasolutions.com>
2016-10-10 15:44:26 -0600

    Update Readme

--- README.md
+++ README.md
@@ -3,12 +3,22 @@ Author: [Ivan Piedra](https://twitter.com/ivapie)
 
 ### Installation
 
-Install the dependencies and devDependencies and run the minify command.
+Install the dependencies and devDependencies and run the convert command for
 
 ```sh
 $ npm install
-$ npm run minifycss
+$ npm run convert
 ```
+
+Run convert command for:
+
+- Convert .scss to css
+- Concat .css
+- Minify .css
+- Clear .css
+- Check for duplicates classes
+- Save a .css (styles.css)
+
 
 License
 ----
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 'use strict';
 let gulp = require("gulp");
-let minifycss = require("gulp-minify-css");
 let cleanCSS = require("gulp-clean-css");
 let concat = require("gulp-concat");
 var dedupe = require('gulp-dedupe');
@@ -10,7 +9,6 @@ gulp.task('sass', function () {
     return gulp.src('./components/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('styles.css'))
-        .pipe(minifycss())
         .pipe(dedupe())
         .pipe(cleanCSS({debug: true}, function(details){
             console.log(details);
